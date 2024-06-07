09a75b7b77ce27448953d801a0e7b0c9624a0a72
Warling-Sebastian <warlinglopez01@gmail.com>
2024-06-07 01:56:51 -0400

    main all
    go

---
+++ src/routes/error.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+        <h1>Lo sentimos warling dice que esa pagina no existe.</h1>
+        <p>para mas info llame al 809-943-9787</p>
+        <h3>wenoooo ponga eso bn</h3>
+    </div>
+  )
+}
+
+export default Error;
